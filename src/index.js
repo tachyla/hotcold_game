245ba2edfdd46b9467b4c9d920979c814bc34145
tachyla <tachylad.murray@gmail.com>
2017-05-15 13:43:38 -0500

    created export default func

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import textBox from './textBox'
+import './index.css';
+
+ReactDOM.render(
+  <textBox />,
+  document.getElementById('root')
+);
---
+++ textBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+
+export default function textBox() {
+    return(
+        <div>
+            <input type='text'> </input>
+            <span>HELLO WORLD</span>
+        </div>
+    )
+};
